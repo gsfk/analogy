5cb51ca83c8c0be700be844ca019fa1f52fbf41d
gsfk <G2@Gordons-MacBook-Pro.local>
2016-04-14 22:54:26 -0400

    added extra properties

--- algebraic_structure.cpp
+++ algebraic_structure.cpp
@@ -1,61 +1,23 @@
-
 #include <vector>
 #include "condition.h"
 #include "algebraic_structure.h"
 
-//debug helper fns
-void print_elements(std::vector<char> v){
-    std::cout << "element vector is: ";
-    
-    for (int i= 0; i < v.size(); i++){
-        std::cout <<"["<< v[i] << "]";
-    }
-    std::cout << std::endl;
-}
-
-void print_facts (std::vector < std::vector<char> > v){
-    std::vector<char> current;
-    std::cout << "facts vector is: ";
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << "{";
-        current = v[i];
-        std::cout << "(" << current[0] << ", " << current[1] << ", " << current[2] << ")";
-    }
-    std::cout << "}"<< std::endl;
-}
+//debug print fns
+void print_elements(std::vector<char>);
+void print_facts (std::vector < std::vector<char> >);
 
 
-void Algebraic_structure::two_ways_of_analogy_search(Condition* c){
-    
-    std::cout << "\nSTART BRUTE FORCE SEARCH for predicate: " << c->name << std::endl;
-    
-    
-    //check all cases
-    //void functions update boolean values associated with this particular predicate
-    
-    //if this one is true, the rest are either redundant or false
-    forall_x_forall_y(c);
-    
-    //for any of these that are true, the corresponding arrangment without "unique"
-    //is also true
-    exists_unique_x_exists_y(c);
-    exists_x_exists_unique_y(c);
-    exists_unique_x_exists_unique_y(c);
-    
-    forall_x_exists_y(c);
-    exists_x_forall_y(c);
-    exists_x_exists_y(c);
-}
-
-
-//table lookup
-//currently implemented for binary operators only (given x and y, returns a result z)
-
-//but I've tried to write everything with the goal of arbitrary arity, so this shouldn't be difficult 
-//to generalize. As a bonus the complexity of C++ vector lookup is faster than I thought (O(1)), so lookup is 
-//faster than expected.  
-
-//TODO: generalize to arbitrary arity
+/*
+ Fact table lookup
+ currently works for binary operators only (facts with three elements)
+ 
+ However it's written in a general style that would not be difficult to expand to arbitrary
+ arity: it simply iterates through facts looking for a match, rather than using a faster
+ implementation that would be specific to a particular arity, the most obvious example being
+ a 2D array akin to a multiplication table for binary operators.
+ 
+ Complexity is not terrible, though, each lookup is linear in the size of the fact table (number of facts * arity)
+ */
 char Algebraic_structure::table_lookup(char a, char b){
     std::vector<char> current_fact;
     
@@ -64,8 +26,6 @@ char Algebraic_structure::table_lookup(char a, char b){
         
         //get fact, which is a vector of size arity
         current_fact = facts[i];
-        
-        //  std::cout << "table lookup: " << current_fact[0] << ", " << current_fact[1] << ", " << current_fact[2] << std::endl;
         
         //pattern match on a fact (a,b,x), return x
         if (current_fact[0] == a && current_fact[1] == b){
@@ -76,8 +36,7 @@ char Algebraic_structure::table_lookup(char a, char b){
     //if you made it here, fact not found
     //input domain is required to be complete and fully definied,
     //so as long as we ask about elements of the domain, we should always find facts
-    //even if the operator does not have the closure property
-
+    
     //TODO: exception?
     std::cerr << "fact not found, x = " << a <<", y = " << b << std::endl;
     return 0;
@@ -108,29 +67,45 @@ char Algebraic_structure::table_lookup(char a, char b){
 
 
 
+//main brute force search function, calls all cases.
+//void function, updates boolean values associated with the condition given as input
+void Algebraic_structure::two_ways_of_analogy_search(Condition* c){
+    forall_x_forall_y(c);
+    exists_unique_x_exists_y(c);
+    exists_x_exists_unique_y(c);
+    exists_unique_x_exists_unique_y(c);
+    forall_x_exists_y(c);
+    exists_x_forall_y(c);
+    exists_x_exists_y(c);
+}
+
+
+//search for extra properties not expressible with only two quantifiers
+void Algebraic_structure::extra_properties_search(){
+    commutativity();
+    associativity();
+    closure();
+    left_identity_left_inverse();
+    left_identity_right_inverse();
+    right_identity_left_inverse();
+    right_identity_right_inverse();
+}
+
+
+
+
+
+
+
 void Algebraic_structure::forall_x_forall_y(Condition* c){
     char x,y;
-    
-    std::cout << " ∀x∀y: " << c->name << std::endl;
-    std::cout << "domain has " << this->order<< " elements" << std::endl;
-    std::cout << " and " << this->facts.size() << " facts" << std::endl;
-    
-    std::cout << "elements: " << std::endl;
-    print_elements(this->elements);
-    
-    std::cout << "facts:" <<std::endl;
-    print_facts(this->facts);
-    
     for (int i = 0; i < this->order; i++){
         x = this->elements[i];
         for (int j = 0; j < this->order; j++){
             y = this->elements[j];
             
-            std::cout<< "checking x = " << x << ", y = " << y << std::endl;
-            
             //check condition, quit if false
             if (!c->check_condition(*this, x, y)){
-                std::cout << "found false for x = " << x << ", y = " << y << std::endl;
                 c->values[FOR_ALL_X_FOR_ALL_Y] = false;
                 return;
             }
@@ -138,14 +113,12 @@ void Algebraic_structure::forall_x_forall_y(Condition* c){
     }
     //made it all the way without quitting, all conditions true
     c->values[FOR_ALL_X_FOR_ALL_Y] = true;
-    std::cout << "for all for all true for " << c->name << std::endl;
 }
 
 
 
 
 //handles both "exists y" and "exists unique y"
-
 void Algebraic_structure::forall_x_exists_y(Condition* c){
     char x,y;
     bool exists, exists_unique;
@@ -163,7 +136,7 @@ void Algebraic_structure::forall_x_exists_y(Condition* c){
         //update to true when it applies
         exists = false;
         
-        //counter for "exists unique" quantifier
+        //inner loop counter for "exists unique" quantifier
         unique_counter = 0;
         
         //inner loop y
@@ -174,6 +147,7 @@ void Algebraic_structure::forall_x_exists_y(Condition* c){
                 unique_counter ++;
             }
         } //end inner loop
+        
         if (!exists) {
             //found a value of x where no y value meets the condition
             //mark exists_y and exists_unique_y as false, exit
@@ -191,14 +165,10 @@ void Algebraic_structure::forall_x_exists_y(Condition* c){
     //if we made it here, "for all x, exists y" is met for this condition
     c->values[FOR_ALL_X_EXISTS_Y] = true;
     
-    //remove
-    // std::cout<< "for all exists found true for " << c->name << std::endl;
-    
     //and possibly also "for all x, exists unique y"
     if (exists_unique) {
         c->values[FOR_ALL_X_EXISTS_UNIQUE_Y] = true;
     }
-    
 }
 
 
@@ -237,9 +207,6 @@ void Algebraic_structure::exists_x_forall_y(Condition* c){
         c->values[EXISTS_UNIQUE_X_FOR_ALL_Y] = true;
     }
 }
-
-
-
 
 
 
@@ -311,9 +278,6 @@ void Algebraic_structure::exists_unique_x_exists_y(Condition* c){
 
 
 
-
-
-
 //there's some x where the condition is met for a single y
 
 void Algebraic_structure::exists_x_exists_unique_y(Condition* c){
@@ -341,13 +305,11 @@ void Algebraic_structure::exists_x_exists_unique_y(Condition* c){
     
     //made it to the end without finding a unique y for any x
     c->values[EXISTS_X_EXISTS_UNIQUE_Y] = false;
-    
 }
 
 
 
 //condition met exactly once
-//should be able to sneak this condition in somewhere else
 
 void Algebraic_structure::exists_unique_x_exists_unique_y(Condition* c){
     char x,y;
@@ -375,17 +337,374 @@ void Algebraic_structure::exists_unique_x_exists_unique_y(Condition* c){
     //made it to the end, xy found either one or zero times
     if (found_unique_xy) {
         c->values[EXISTS_UNIQUE_X_EXISTS_UNIQUE_Y] = true;
-        //std::cout << "unique_unique found true for " << c->name <<std::endl;
     } else {
         c->values[EXISTS_UNIQUE_X_EXISTS_UNIQUE_Y] = false;
-        //std::cout << "unique_unique found false for " << c->name <<std::endl;
+    }
+}
+
+//end of double quantifier cases
+
+
+
+//interesting triple quantifier cases:
+
+void Algebraic_structure::commutativity(){
+    char x,y;
+    bool commutes = false;
+    for (int i = 0; i < this->order; i++){
+        x = this->elements[i];
+        for (int j = 0; j < this->order; j++){
+            y = this->elements[j];
+            
+            //check commutativity, quit if false
+            commutes = (table_lookup(x,y) == table_lookup(y,x));
+            if (!commutes){
+                this->extra_properties[COMMUTATIVITY] = false;
+                return;
+            }
+        }
+    }
+    //else true
+    this->extra_properties[COMMUTATIVITY] = true;
+}
+
+void Algebraic_structure::associativity(){
+    char x,y,z, left, right;
+    bool associative = false;
+    
+    //triple nested loop, check x(yz) = (xy)z for all x,y,z
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        for (int j=0; j< this->order; j++){
+            y = this->elements[j];
+            for (int k = 0; k< this->order; k++){
+                z = this->elements[k];
+                left = table_lookup(x, table_lookup(y, z));     //x(yz)
+                right = table_lookup(table_lookup(x,y),z);      //(xy)z
+                associative = (left == right);
+                
+                if(!associative){
+                    this->extra_properties[ASSOCIATIVITY] = false;
+                    return;
+                }
+            } //end z
+        }//end y
+    }// end x
+    
+    //else true
+    this->extra_properties[ASSOCIATIVITY] = true;
+}
+
+//check that x * y results in one of the member elements
+void Algebraic_structure::closure(){
+    char x,y,z, element;
+    bool found_element;
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        for (int j=0; j< this->order; j++){
+            y = this->elements[j];
+            
+            //z = x * y
+            z = table_lookup(x,y);
+            found_element = false;
+            
+            //check membership of z
+            for (int k= 0; k < this->order; k++){
+                element = this->elements[k];
+                if (z == element){
+                    found_element = true;
+                    break;
+                }
+            }//end z
+            
+            //if z not found in elements for this x*y
+            if (found_element == false){
+                this->extra_properties[CLOSURE] = false;
+                return;
+            }
+        }//end y
+        
+    }//end x
+    this->extra_properties[CLOSURE] = true;
+}
+
+
+
+//existence of identity elements is already checked by the two quantifier case
+//these are simple helper functions to find which particular element is identity
+//needed for finding inverses
+
+bool Algebraic_structure::is_left_identity_element(char iden){
+    char y;
+    for (int i= 0; i < this->order; i++){
+        y = this->elements[i];
+        
+        //if you find a non-identity case, return false
+        if (!(table_lookup(iden,y) == y)){
+            return false;
+        }
+    }
+    //else return true
+    return true;
+}
+
+bool Algebraic_structure::is_right_identity_element(char iden){
+    char y;
+    for (int i= 0; i < this->order; i++){
+        y = this->elements[i];
+        
+        //if you find a non-identity case, return false
+        if (!(table_lookup(y,iden) == y)){
+            return false;
+        }
+    }
+    //else return true
+    return true;
+}
+
+
+
+//existence of left inverse: ∃x ∀y ∃z (x is identity & z * y = x)
+void Algebraic_structure::left_identity_left_inverse(){
+    char x,y,z;
+    char left_iden = '0'; //dummy initialization
+    bool found_left_identity = false;
+    bool is_inverse = false;
+    int y_counter = 0;
+    
+    //is x an identity element?
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        found_left_identity = is_left_identity_element(x);
+        
+        if (found_left_identity){
+            left_iden = x;
+        }
+    }
+    
+    //quit if you never find an identity element
+    if (!found_left_identity){
+        this->extra_properties[LEFT_IDENTITY_LEFT_INVERSE] = false;
+        return;
+    }
+    
+    
+    for (int j=0; j< this->order; j++){
+        y = this->elements[j];
+        
+        for (int k=0; k< this->order; k++){
+            z = this->elements[k];
+            
+            is_inverse = (table_lookup(z,y) == left_iden);
+            
+            //if you find an inverse for this y, increment counter and move to the next one
+            if (is_inverse){
+                y_counter++;
+                break;
+            }
+            
+        }//end z
+    }//end y
+    
+    //true if some inverse found for all y
+    if (y_counter == this->order){
+        this->extra_properties[LEFT_IDENTITY_LEFT_INVERSE] = true;
+        
+    } else {
+        this->extra_properties[LEFT_IDENTITY_LEFT_INVERSE] = false;
         
     }
+    
+}
+
+
+
+//existence of left inverse: ∃x ∀y ∃z (x is identity & y * z = x)
+void Algebraic_structure::left_identity_right_inverse(){
+    char x,y,z;
+    char left_iden = '0'; //dummy initialization
+    bool found_left_identity = false;
+    bool is_inverse = false;
+    int y_counter = 0;
+    
+    //is x an identity element?
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        found_left_identity = is_left_identity_element(x);
+        if (found_left_identity){
+            left_iden = x;
+        }
+    }
+    
+    //quit if you never find an identity element
+    if (!found_left_identity){
+        this->extra_properties[LEFT_IDENTITY_RIGHT_INVERSE] = false;
+        return;
+    }
+    
+    for (int j=0; j< this->order; j++){
+        y = this->elements[j];
+        
+        for (int k=0; k< this->order; k++){
+            z = this->elements[k];
+            
+            is_inverse = (table_lookup(y, z) == left_iden);
+            
+            //if you find an inverse for this y, increment counter and move to the next one
+            if (is_inverse){
+                y_counter++;
+                break;
+            }
+        }//end z
+        
+    }//end y
+    
+    //true if some inverse found for all y
+    if (y_counter == this->order){
+        this->extra_properties[LEFT_IDENTITY_RIGHT_INVERSE] = true;
+        
+    } else {
+        this->extra_properties[LEFT_IDENTITY_RIGHT_INVERSE] = false;
+        
+    }
+    
 }
 
 
 
 
+//existence of left inverse: ∃x ∀y ∃z (x is identity & z * y = x)
+void Algebraic_structure::right_identity_left_inverse(){
+    char x,y,z;
+    char right_iden = '0'; //dummy initialization
+    bool found_right_identity = false;
+    bool is_inverse = false;
+    int y_counter = 0;
+    
+    //is x an identity element?
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        found_right_identity = is_right_identity_element(x);
+        if (found_right_identity){
+            right_iden = x;
+        }
+    }
+    
+    //quit if you never find an identity element
+    if (!found_right_identity){
+        this->extra_properties[RIGHT_IDENTITY_LEFT_INVERSE] = false;
+        return;
+    }
+    
+    
+    for (int j=0; j< this->order; j++){
+        y = this->elements[j];
+        
+        for (int k=0; k< this->order; k++){
+            z = this->elements[k];
+            
+            is_inverse = (table_lookup(z,y) == right_iden);
+            
+            //if you find an inverse for this y, increment counter and move to the next one
+            if (is_inverse){
+                y_counter++;
+                break;
+            }
+            
+        }//end z
+    }//end y
+    
+    //true if some inverse found for all y
+    if (y_counter == this->order){
+        this->extra_properties[RIGHT_IDENTITY_LEFT_INVERSE] = true;
+        
+    } else {
+        this->extra_properties[RIGHT_IDENTITY_LEFT_INVERSE] = false;
+        
+    }
+    
+}
+
+
+
+//existence of left inverse: ∃x ∀y ∃z (x is identity & y * z = x)
+void Algebraic_structure::right_identity_right_inverse(){
+    char x,y,z;
+    char right_iden = '0'; //dummy initialization
+    bool found_right_identity = false;
+    bool is_inverse = false;
+    int y_counter = 0;
+    
+    //is x an identity element?
+    for (int i = 0; i<this->order; i++ ){
+        x = this->elements[i];
+        found_right_identity = is_left_identity_element(x);
+        if (found_right_identity){
+            right_iden = x;
+        }
+    }
+    
+    //quit if you never find an identity element
+    if (!found_right_identity){
+        this->extra_properties[RIGHT_IDENTITY_RIGHT_INVERSE] = false;
+        return;
+    }
+    
+    for (int j=0; j< this->order; j++){
+        y = this->elements[j];
+        y_counter = 0;
+        
+        for (int k=0; k< this->order; k++){
+            z = this->elements[k];
+            
+            is_inverse = (table_lookup(y, z) == right_iden);
+                        
+            //if you find an inverse for this y, increment counter and move to the next one
+            if (is_inverse){
+                y_counter++;
+                break;
+            }
+        }//end z
+        
+    }//end y
+    
+    //true if some inverse found for all y
+    if (y_counter == this->order){
+        this->extra_properties[RIGHT_IDENTITY_RIGHT_INVERSE] = true;
+        
+    } else {
+        this->extra_properties[RIGHT_IDENTITY_RIGHT_INVERSE] = false;
+        
+    }
+    
+}
+//end looping code
+
+
+
+
+
+
+
+
+//debug helper fns
+void print_elements(std::vector<char> v){
+    std::cout << "element vector is: ";
+    for (int i= 0; i < v.size(); i++){
+        std::cout <<"["<< v[i] << "]";
+    }
+    std::cout << std::endl;
+}
+
+void print_facts (std::vector < std::vector<char> > v){
+    std::vector<char> current;
+    std::cout << "facts vector is: {";
+    for (int i = 0; i < v.size(); i++) {
+        current = v[i];
+        std::cout << "(" << current[0] << ", " << current[1] << ", " << current[2] << ")";
+    }
+    std::cout << "}"<< std::endl;
+}
 
 
 
--- algebraic_structure.h
+++ algebraic_structure.h
@@ -1,24 +1,51 @@
 #ifndef algebraic_structure_h
 #define algebraic_structure_h
 
-#include "algebraic_structure.h"
 #include <vector>
 #include <string>
 #include <iostream>
+#include <map>
 
-//#include "condition.h"
 class Condition;    //forward declaration to resolve circular dependency
 
+enum Properties {
+    ASSOCIATIVITY,
+    COMMUTATIVITY,
+    CLOSURE,
+    LEFT_IDENTITY_LEFT_INVERSE,
+    RIGHT_IDENTITY_LEFT_INVERSE,
+    LEFT_IDENTITY_RIGHT_INVERSE,
+    RIGHT_IDENTITY_RIGHT_INVERSE,
+};
+
+
+
+/*
+ Class to represent input domain, stores the elements and fact table from input,
+ and a few other properties of the domain.
+ */
 
 class Algebraic_structure {
 private:
+    
+    //domain elements
     std::vector<char> elements;
-    int order;   //number of elements
-    std::string relation;
+    
+    //number of elements
+    int order;
+    
+    //fact table generated from input facts
     std::vector < std::vector<char> > facts;
     unsigned long num_facts;
     
+    //relational properties not tied to a particular condition
+    //commutativity, associativity, inverses, etc.
+    std::map<Properties, bool> extra_properties;
+    
+    
 public:
+    std::string relation;
+    
     //constructors
     Algebraic_structure(){};
     
@@ -31,8 +58,11 @@ public:
         std::cout << "called Alg constructor: " << elements.size() << " elements, relation '" << relation << "' and " << facts.size() << " facts. " << std::endl;
     };
     
+    char table_lookup(char, char);
     
-    void two_ways_of_analogy_search(Condition* c);
+    
+    //Brute force checks of all two quantifier cases
+    void two_ways_of_analogy_search(Condition*);
     
     void forall_x_forall_y(Condition*);
     
@@ -41,14 +71,38 @@ public:
     void exists_x_forall_y(Condition*);
     
     void exists_x_exists_y(Condition*);
-    
+
     void exists_x_exists_unique_y(Condition*);
     
     void exists_unique_x_exists_y(Condition*);
     
     void exists_unique_x_exists_unique_y(Condition*);
     
-    char table_lookup(char x, char y);
+    
+    
+    //select properties requiring greater than two quantifiers
+    void extra_properties_search();
+    
+    void commutativity();
+    
+    void associativity();
+    
+    void closure();
+    
+    void left_identity_left_inverse();
+    
+    void left_identity_right_inverse();
+
+    void right_identity_left_inverse();
+    
+    void right_identity_right_inverse();
+    
+    
+    
+    //helper functions
+    bool is_left_identity_element(char iden);
+    
+    bool is_right_identity_element(char iden);
 };
 
 
